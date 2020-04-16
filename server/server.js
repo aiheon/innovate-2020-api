0cb9e5b4c40558f307bcb579c47e2078dddc8d2f
aiheon <aiheon@coveo.com>
2020-04-15 21:56:04 -0400

    clean up for routes

--- server/server.js
+++ server/server.js
@@ -1,11 +1,21 @@
 const express = require('express');
-const apiRouter = require('./routes');
 
+// routes
+const projectsRoute = require('./routes/projects');
+
+
+// app using
 const app = express();
-
 app.use(express.json());
-app.use('/api/projects', apiRouter);
 
+
+// use routes
+app.use('/api/projects', projectsRoute);
+
+
+
+
+ // listening to port 3000
 app.listen(process.env.PORT || '3000', () => {
   console.log(`Server is runnig on port: ${process.env.PORT || '3000'}`);
 });
